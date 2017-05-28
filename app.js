a22b8ad92cce387b24180a06a99bc628e3645f1f
NITIN KUMAR SINGH <nitinksingh11@gmail.com>
2017-05-28 20:12:53 +0530

    structure code in terms of fn-1

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 var request = require("request");
 
-request({
-  uri: "dhttps://obscure-sea-45533.herokuapp.com/api/customersa",
-  method: "GET",
-  timeout: 10000,
-  followRedirect: true,
-  maxRedirects: 10
-}, function(error, response, body) {
-  if(error){
-    console.log(error);
+var call = function(error, response, body) {
+                if(error){
+                  console.log(error);
+                }
+                console.log(body);
+              };
+var getoption = {
+                uri: "https://obscure-sea-45533.herokuapp.com/api/customers",
+                method: "GET",
+                timeout: 10000,
+                followRedirect: true,
+                maxRedirects: 10
+              } ;
+request(getoption,call);
+
+/*
+module.exports = {
+  getRequest: function () {
+       //   request(getoption,call);
+  },
+  postRequest: function () {
+    // whatever
   }
-  console.log(body);
-});
+};
+*/
